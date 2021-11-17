ec13dc65a98ab7add6256e79b011ecd7fcb06bf6
김민성 <kmsz5813@naver.com>
2021-11-17 15:17:17 +0900

    로그인 버튼 눌리면 -> email / pw 변수에 저장

--- app/src/main/java/com/nepplus/colosseum_20211117/MainActivity.kt
+++ app/src/main/java/com/nepplus/colosseum_20211117/MainActivity.kt
@@ -11,13 +11,23 @@ class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setupEvents()
         setValues()
 
 
     }
+
     override fun setupEvents() {
+
+        binding.btnLogin.setOnClickListener {
+
+            val inputEmail = binding.edtEmail.text.toString()
+            val inputPw = binding.edtPassword.text.toString()
+
+//            서버에서 이메일 / 비번이 맞는 계정인지? 로그인 요청
+
+        }
 
     }
 
