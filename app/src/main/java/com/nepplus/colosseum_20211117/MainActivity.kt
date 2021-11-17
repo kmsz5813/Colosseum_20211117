213fc73519fbb6f3882651a4680de2dba6fce5be
김민성 <kmsz5813@naver.com>
2021-11-17 14:50:58 +0900

    binding 변수 생성 / 초기화

--- app/src/main/java/com/nepplus/colosseum_20211117/MainActivity.kt
+++ app/src/main/java/com/nepplus/colosseum_20211117/MainActivity.kt
@@ -2,10 +2,15 @@ package com.nepplus.colosseum_20211117
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.colosseum_20211117.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
